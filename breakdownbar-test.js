3c3f5b5835f23abc8fad9c5d9558baf92d931f7e
Peter Jacobs <peter.jacobs@ambassador-enterprises.com>
2014-01-22 11:08:31 -0500

    Update sample page

--- breakdownbar-test.js
+++ breakdownbar-test.js
@@ -1,79 +1,79 @@
 requirejs.config({
-    paths: {
-        jquery: 'lib/jquery',
-        underscore: 'lib/underscore'
-    },
-    shim: {
-        underscore: {
-            exports: '_'
-        }
-    }
+	paths: {
+		jquery: 'lib/jquery',
+		underscore: 'lib/underscore'
+	},
+	shim: {
+		underscore: {
+			exports: '_'
+		}
+	}
 });
 
 
 define([
-    'jquery',
-    'breakdownbar'
+	'jquery',
+	'breakdownbar'
 ], function($,BreakdownBar) {
 
-    var parts = [];
+	var parts = [];
 
-    for(var i = 1; i <= 3; i++) {
-        parts.push({
-            width: (i*10)
-        });
-    }
+	for(var i = 1; i <= 3; i++) {
+		parts.push({
+			progress: (i*10)
+		});
+	}
 
-    var bar = new BreakdownBar({
-        parts: parts
-    });
+	var bar = new BreakdownBar({
+		parts: parts
+	});
 
-    $('.test').append(bar.render().$el);
+	$('.test').append(bar.render().$el);
 
-    $('#randomize').on('click', function() {
-        var remaining = 50 + (Math.random() * 50);
-        for(var i in parts) {
-            var p = parts[i];
-            p.width = Math.random() * remaining;
-            remaining -= p.width;
-        }
-        bar.render();
-    });
+	$('#randomize').on('click', function() {
+		for(var i in parts) {
+			var p = parts[i];
+			p.progress = Math.random() * 100;
+		}
+		bar.render();
+	});
 
-    var parts2 = [];
-    var colors = {
-        15: 'red',
-        25: 'yellow',
-        40: 'green'
-    };
+	var parts2 = [];
+	var colors = {
+		33: 'red',
+		66: 'yellow',
+		100: 'green'
+	};
 
-    for(var i = 1; i <= 3; i++) {
-        parts2.push({
-            value: 50,
-            width: function() {
-                this.value = 10 + (Math.random() * 25);
-                return this.value;
-            },
-            partClass: function() {
-                var keys = Object.keys(colors);
-                for(var i in keys) {
-                    if(this.value <= keys[i]) {
-                        return colors[keys[i]];
-                    } else
-                        continue;
-                }
-            }
-        });
-    }
+	for(var i = 1; i <= 3; i++) {
+		parts2.push({
+			value: 50,
+			progress: function() {
+				this.value = Math.random() * 100;
+				return this.value;
+			},
+			attributes: {
+				class: function() {
+					var keys = Object.keys(colors);
+					for(var i in keys) {
+						if(this.value <= keys[i]) {
+							return colors[keys[i]];
+						} else
+							continue;
+					}
+				}
+			}
+		});
+	}
 
-    var bar2 = new BreakdownBar({
-        parts: parts2
-    });
+	var bar2 = new BreakdownBar({
+		parts: parts2
+	});
 
-    $('.test2').append(bar2.render().$el);
+	$('.test2').append(bar2.render().$el);
 
-    $('#randomize2').on('click', function() {
-        bar2.render();
-    });
+	$('#randomize2').on('click', function() {
+		bar2.render();
+	});
 
 });
